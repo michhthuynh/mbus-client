8991f04255fbe37f201ec903c6bd9b6d6f35ee37
DESKTOP-G40T55B\michh <mich.hthuynh@gmail.com>
2021-06-14 22:48:20 +0700

    update api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://13.57.247.71:5000/api",
   responseType: "json",
   headers: {
     "Content-Type": "application/json"
